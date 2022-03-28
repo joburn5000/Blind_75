9aee9574d90f92dc72be15352573e7e19bfff777
joburn5000 <86541546+joburn5000@users.noreply.github.com>
2022-03-28 11:47:19 -0500

    Update Sum_of_Two_Integers.py

--- binary/Sum_of_Two_Integers.py
+++ binary/Sum_of_Two_Integers.py
@@ -15,3 +15,36 @@ import numpy as np
 class Solution:
     def getSum(self, a: int, b: int) -> int:
         return int(math.log2(2**a * 2**b))
+
+# sum() method
+
+class Solution2:
+    def getSum(self, a: int, b: int) -> int:
+        return sum([a,b])
+
+# full adder method
+
+import numpy as np
+
+class Solution3:
+    def getSum(self, a: int, b: int) -> int:
+        carry = 0
+        s = ""
+        a = int(np.binary_repr(a,11),2)                             # two's complement
+        b = int(np.binary_repr(b,11),2)
+        
+        while a or b:
+            s = ''.join(('',s,str(int(a%2^b%2^carry))))             # increase sum
+            carry = (a%2 & b%2) or (a%2 & carry) or (carry & b%2)   # calculate carry bit
+            a = int(a/2)                                            # go to next bit
+            b = int(b/2)
+            
+        s = ''.join(('',s,str(carry)))                              # final carry bit
+        
+        negative = False
+        if len(s)>10:
+            negative = (s[10]=="1")                                 # check if number is negative
+        
+        output = int(s[::-1],2) % 1024
+        return output-1024 if negative else output
+   
