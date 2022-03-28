d8f454c2d037ad3cbf84560412293792ecdbbffa
joburn5000 <86541546+joburn5000@users.noreply.github.com>
2022-03-28 12:28:06 -0500

    Update Sum_of_Two_Integers.py

--- binary/Sum_of_Two_Integers.py
+++ binary/Sum_of_Two_Integers.py
@@ -5,7 +5,10 @@ Difficulty: Medium
 
 Given two integers a and b, return the sum of the two integers without using the operators + and -.
 
-Completed: 3/27/2022
+Completed:
+    exponent method:                3/27/2022
+    sum() and full adder method:    3/28/2022
+    
 """
 
 # exponent method
