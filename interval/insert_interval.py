5090ab560ebdf2bb88f46e094df8928db006e454
joburn5000 <86541546+joburn5000@users.noreply.github.com>
2022-06-16 20:53:31 -0500

    Added time complexity and space complexity

--- interval/insert_interval.py
+++ interval/insert_interval.py
@@ -1,9 +1,12 @@
 """
 
+You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] 
+represent the start and the end of the ith interval and intervals is sorted in ascending order by 
+starti. You are also given an interval newInterval = [start, end] that represents the start and 
+end of another interval.
 
-You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
-
-Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+Insert newInterval into intervals such that intervals is still sorted in ascending order by starti 
+and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
 
 Return intervals after the insertion.
 
@@ -76,6 +79,8 @@ that bound in merged (4). If the newInterval[1] is within the bounds, merge usin
 the new interval is completely to the right of all the other bounds, then just return preceding plus 
 newInterval (7). This covers all possible cases.
 
+Time Complexity: O(N*log(N))
+Space Complexity: O(N)
 
 """
       
